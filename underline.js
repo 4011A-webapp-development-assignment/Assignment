eecc6e2341cd0413800e31d33b57d6ac63274a41
cg1218 <fke22pru@uea.ac.uk>
2023-11-26 23:04:24 +0000

    increase goalMainText mobile readability

--- underline.js
+++ underline.js
@@ -1,2 +1,6 @@
 document.querySelectorAll('a[href]').forEach(it => it.style.textDecoration='underline');
-// adds underline to every link in the site
+/* adds underline to every link in the site
+* made by mdaniel
+* https://news.ycombinator.com/item?id=33926974
+*
+* */
